ed3da27ea66759e75c024dc2ef05babd4b0c12d4
Lasantha Kasun <lasantha.k@omobio.net>
2023-04-02 08:00:15 +0530

    textfield-cmp
    create-textfield-component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 import store from "../store/store";
 import { Provider } from "react-redux";
 import "@/styles/main.scss";
+import "@/styles/button.scss";
+import "@/styles/text_field.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
